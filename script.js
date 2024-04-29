45c240354603bbbbc06c2af66e9774136c8f0375
abbxz <151005229+abbxz@users.noreply.github.com>
2024-04-29 23:28:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const days=document.getElementById('days');
+const hours=document.getElementById('hours');
+const minutes=document.getElementById('minutes');
+const seconds=document.getElementById('seconds');
+const year=document.getElementById('year');
+const countdown=document.getElementById('countdown');
+
+const currentyear=new Date().getFullYear();
+const nextyear=currentyear+1;
+
+let newYearTime=new Date(`January 01 ${nextyear} 00:00:00`);
+function update()
+{
+    const currentTime=new Date();
+    const diff=newYearTime-currentTime;
+    let d=Math.floor(diff/1000/60/60/24);
+    let h=Math.floor(diff/1000/60/60)%24;
+    let m=Math.floor(diff/1000/60)%60;
+    let s=Math.floor(diff/1000)%60;
+
+    days.innerHTML=d<10 ? "0"+d:d;
+    hours.innerHTML=h<10? "0"+h:h;
+    minutes.innerHTML=m<10? "0"+m:m;
+    seconds.innerHTML=s<10? "0"+s:s;
+}
+setInterval(update,1000);
